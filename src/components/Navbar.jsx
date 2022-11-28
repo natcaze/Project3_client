598803ac10a6912b5e5d63195b1d76ee60c8ed05
Nathalie Cazemajou <nat.caze@gmail.com>
2022-11-28 12:14:16 +0000

    Edit profile done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,42 +1,37 @@
-import React from 'react'
- import { Link } from 'react-router-dom' 
- import { useContext } from 'react';
-import { AuthContext } from '../contexts/auth.context';
+import React from "react";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../contexts/auth.context";
 
 function Navbar() {
-    const { loggedIn, logout } = useContext(AuthContext);
+  const { loggedIn, logout } = useContext(AuthContext);
   return (
     <div>
-    <nav>
-    <Link to="/">
-        HOME
-    </Link>
-    <Link to="/pages/cocktail">
-        COCKTAIL
-    </Link>
-    <Link to="/pages/articles">
-        ARTICLES
-    </Link>
+      <nav>
+        <Link to="/">HOME</Link>
+        <Link to="/pages/cocktail">COCKTAIL</Link>
+        <Link to="/pages/articles">ARTICLES</Link>
 
-    {loggedIn && (
-        <>
+        {!loggedIn && (
+          <>
             <Link to="/pages/favorites">FAVORITES</Link>
             <Link to="/pages/edit-profile">PROFILE </Link>
             <Link to="/pages/creations">CREATIONS</Link>
-            <Link to="/" onClick={logout}>LOG OUT</Link>
-        </>
-      )}
+            <Link to="/" onClick={logout}>
+              LOG OUT
+            </Link>
+          </>
+        )}
 
-      {!loggedIn && (
-        <>
+        {loggedIn && (
+          <>
             <Link to="/pages/login">LOGIN</Link>
             <Link to="/pages/signup">SIGN UP</Link>
-        </>
-      )}
-    
-    </nav>
+          </>
+        )}
+      </nav>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- src/pages/EditProfile.jsx
+++ src/pages/EditProfile.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
 function EditProfile() {
   return (
-    <div>EditProfile</div>
-  )
+    <div>
+      <h3>Edit Profile</h3>
+      <form>
+        <label>First Name:</label>
+        <input type="text" />
+        <label>Last Name:</label>
+        <input type="text" />
+        <label>Password:</label>
+        <input type="password" />
+      </form>
+    </div>
+  );
 }
 
-export default EditProfile
+export default EditProfile;
