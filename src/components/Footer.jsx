d7b96afbb252e5328b2289c312b3c8229af582d1
Gabriela Meirelles <gb.meirellesmartins@gmail.com>
2022-11-28 17:35:54 +0000

    Footer styled done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,38 @@
 import React from 'react'
 import {Link} from 'react-router-dom';
+import styled from "styled-components";
 
 function Footer() {
   return (
     <div>
+    <StyledFooter>
         <p>Created by Gabriela Meirelles and Nathalie Cazemajou</p>
         <Link to='/contact-us'>Contact Us </Link>
+    </StyledFooter>
     </div>
   )
 }
+
+const StyledFooter = styled.footer`
+height: 6vh;
+background-color: #676FA3;
+display: flex;
+align-items: center;
+
+p{
+  color: white;
+  margin-left: 1vh;
+}
+
+a {
+    text-decoration: none;
+    color: white;
+    margin-left: 10px;
+  }
+  a:hover,
+  .active {
+    color: black;
+  }
+`;
 
 export default Footer
