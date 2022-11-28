761be3e2f6273e722d772f034c6095926ed04635
Nathalie Cazemajou <nat.caze@gmail.com>
2022-11-28 17:32:47 +0000

    random cocktail done

--- src/pages/Cocktail.jsx
+++ src/pages/Cocktail.jsx
@@ -1,84 +1,203 @@
-import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function Cocktail() {
+  const [cocktail, setCocktail] = useState(null);
+  const [ingredients, setIngredients] = useState(null);
+  const getRandom = async () => {
+    try {
+      const response = await axios.get(
+        `${process.env.REACT_APP_API_URL}/api/random`
+      );
+      setCocktail(response.data[0]);
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleIngredients = (e) => setIngredients();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    /*     console.log(e.target.ingredients);
+
+    const checkedFilters = filter() */
+  };
+
   return (
     <div>
       <div>
         <h4>Cocktail Name</h4>
-        {/* <img src="" alt="" /> */}
-        <form>
-          <button type="submit">Random</button>
-        </form>
+        <img src="" alt="" />
+
+        <button onClick={getRandom}>Random</button>
+        {cocktail && (
+          <div>
+            <p>{cocktail.strDrink}</p>
+            <img src={cocktail.strDrinkThumb} alt="" />
+          </div>
+        )}
       </div>
-      <div>
+      <form onSubmit={handleSubmit}>
         <div>
-          <h5>FILTER BY TYPE</h5>
           <div>
+            <h5>FILTER BY TYPE</h5>
             <div>
-              <input type="checkbox" />
-              <label> Rum </label>
-              <input type="checkbox" />
-              <label> Vodka </label>
-              <input type="checkbox" />
-              <label> Gin </label>
+              <div>
+                <input
+                  type="checkbox"
+                  name="ingredients"
+                  value="Rum"
+                  onChange={handleIngredients}
+                />
+                <label> Rum </label>
+                <input
+                  type="checkbox"
+                  name="ingredients"
+                  value="Vodka"
+                  onChange={handleIngredients}
+                />
+                <label> Vodka </label>
+                <input
+                  type="checkbox"
+                  name="ingredients"
+                  value="Gin"
+                  onChange={handleIngredients}
+                />
+                <label> Gin </label>
+              </div>
+              <div>
+                <input
+                  type="checkbox"
+                  name="ingredients"
+                  value="Tequila"
+                  onChange={handleIngredients}
+                />
+                <label> Tequila </label>
+                <input
+                  type="checkbox"
+                  name="ingredients"
+                  value="Liqueur"
+                  onChange={handleIngredients}
+                />
+                <label> Liqueur </label>
+                <input
+                  type="checkbox"
+                  name="ingredients"
+                  value="Others"
+                  onChange={handleIngredients}
+                />
+                <label> Others </label>
+              </div>
             </div>
+          </div>
+
+          <div>
+            <h5>FILTER BY ALCOHOL</h5>
             <div>
-              <input type="checkbox" />
-              <label> Tequila </label>
-              <input type="checkbox" />
-              <label> Liqueur </label>
-              <input type="checkbox" />
-              <label> Others </label>
+              <div>
+                <input
+                  type="checkbox"
+                  name="ingredients"
+                  value="Rum"
+                  onChange={handleIngredients}
+                />
+                <label> Rum </label>
+                <input
+                  type="checkbox"
+                  name="ingredients"
+                  value="Vodka"
+                  onChange={handleIngredients}
+                />
+                <label> Vodka </label>
+                <input
+                  type="checkbox"
+                  name="ingredients"
+                  value="Gin"
+                  onChange={handleIngredients}
+                />
+                <label> Gin </label>
+              </div>
+              <div>
+                <input
+                  type="checkbox"
+                  name="ingredients"
+                  value="Tequila"
+                  onChange={handleIngredients}
+                />
+                <label> Tequila </label>
+                <input
+                  type="checkbox"
+                  name="ingredients"
+                  value="Liqueur"
+                  onChange={handleIngredients}
+                />
+                <label> Liqueur </label>
+                <input
+                  type="checkbox"
+                  name="ingredients"
+                  value="Others"
+                  onChange={handleIngredients}
+                />
+                <label> Others </label>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <h5>FILTER BY INGREDIENTS</h5>
+            <div>
+              <div>
+                <input
+                  type="checkbox"
+                  name="ingredients"
+                  value="Rum"
+                  onChange={handleIngredients}
+                />
+                <label> Rum </label>
+                <input
+                  type="checkbox"
+                  name="ingredients"
+                  value="Vodka"
+                  onChange={handleIngredients}
+                />
+                <label> Vodka </label>
+                <input
+                  type="checkbox"
+                  name="ingredients"
+                  value="Gin"
+                  onChange={handleIngredients}
+                />
+                <label> Gin </label>
+              </div>
+              <div>
+                <input
+                  type="checkbox"
+                  name="ingredients"
+                  value="Tequila"
+                  onChange={handleIngredients}
+                />
+                <label> Tequila </label>
+                <input
+                  type="checkbox"
+                  name="ingredients"
+                  value="Liqueur"
+                  onChange={handleIngredients}
+                />
+                <label> Liqueur </label>
+                <input
+                  type="checkbox"
+                  name="ingredients"
+                  value="Others"
+                  onChange={handleIngredients}
+                />
+                <label> Others </label>
+              </div>
             </div>
           </div>
         </div>
 
-        <div>
-          <h5>FILTER BY ALCOHOL</h5>
-          <div>
-            <div>
-              <input type="checkbox" />
-              <label> Rum </label>
-              <input type="checkbox" />
-              <label> Vodka </label>
-              <input type="checkbox" />
-              <label> Gin </label>
-            </div>
-            <div>
-              <input type="checkbox" />
-              <label> Tequila </label>
-              <input type="checkbox" />
-              <label> Liqueur </label>
-              <input type="checkbox" />
-              <label> Others </label>
-            </div>
-          </div>
-        </div>
-
-        <div>
-          <h5>FILTER BY INGREDIENTS</h5>
-          <div>
-            <div>
-              <input type="checkbox" />
-              <label> Rum </label>
-              <input type="checkbox" />
-              <label> Vodka </label>
-              <input type="checkbox" />
-              <label> Gin </label>
-            </div>
-            <div>
-              <input type="checkbox" />
-              <label> Tequila </label>
-              <input type="checkbox" />
-              <label> Liqueur </label>
-              <input type="checkbox" />
-              <label> Others </label>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <form>
         <button type="submit">Choose another</button>
       </form>
     </div>
---
+++ src/pages/EditArticle.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function EditArticle() {
+  return (
+    <div>
+    <h3>EDIT ARTICLE</h3>
+    <form action=""></form>
+    </div>
+  )
+}
+
+export default EditArticle
