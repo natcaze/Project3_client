13034191657aea117353b10d6e4255ea8431b1e5
Nathalie Cazemajou <nat.caze@gmail.com>
2022-11-28 12:31:55 +0000

    create article and create cocktail pages done

--- src/pages/CreateArticle.jsx
+++ src/pages/CreateArticle.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import Creations from "./Creations";
 
 function CreateArticle() {
   return (
-    <div>CreateArticle</div>
-  )
+    <div>
+      <h3>CREATE ARTICLE</h3>
+      <form onSubmit={<Creations />}>
+        <label> TITLE </label>
+        <input type="text" />
+        <label> DESCRIPTION </label>
+        <input type="text" />
+        {/* <img src="" alt="" /> */}
+        <button type="submit">ADD</button>
+      </form>
+    </div>
+  );
 }
 
-export default CreateArticle
+export default CreateArticle;
--- src/pages/CreateCocktail.jsx
+++ src/pages/CreateCocktail.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import Creations from './Creations';
 
 function CreateCocktail() {
   return (
-    <div>CreateCocktail</div>
-  )
+    <div>
+      <h3>CREATE YOUR COCKTAIL</h3>
+      <form onSubmit={<Creations/>}>
+<label>COCKTAIL NAME</label>
+<input type="text" />
+<label>DESCRIPTION</label>
+<input type="text" />
+<label>INGREDIENTS</label>
+<input type="text" />
+{/* <img src="" alt="" /> */}
+<button type='submit'>ADD</button>
+      </form>
+
+    </div>
+  );
 }
 
 export default CreateCocktail
