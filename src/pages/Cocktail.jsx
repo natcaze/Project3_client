a059016d4f9aa404a243094487abfe94aaab763d
Nathalie Cazemajou <nat.caze@gmail.com>
2022-11-28 14:24:50 +0000

    Cocktail page done

--- src/pages/Cocktail.jsx
+++ src/pages/Cocktail.jsx
@@ -1,9 +1,88 @@
-import React from 'react'
+import React from "react";
 
 function Cocktail() {
   return (
-    <div>CocktailPage</div>
-  )
+    <div>
+      <div>
+        <h4>Cocktail Name</h4>
+        {/* <img src="" alt="" /> */}
+        <form>
+          <button type="submit">Random</button>
+        </form>
+      </div>
+      <div>
+        <div>
+          <h5>FILTER BY TYPE</h5>
+          <div>
+            <div>
+              <input type="checkbox" />
+              <label> Rum </label>
+              <input type="checkbox" />
+              <label> Vodka </label>
+              <input type="checkbox" />
+              <label> Gin </label>
+            </div>
+            <div>
+              <input type="checkbox" />
+              <label> Tequila </label>
+              <input type="checkbox" />
+              <label> Liqueur </label>
+              <input type="checkbox" />
+              <label> Others </label>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <h5>FILTER BY ALCOHOL</h5>
+          <div>
+            <div>
+              <input type="checkbox" />
+              <label> Rum </label>
+              <input type="checkbox" />
+              <label> Vodka </label>
+              <input type="checkbox" />
+              <label> Gin </label>
+            </div>
+            <div>
+              <input type="checkbox" />
+              <label> Tequila </label>
+              <input type="checkbox" />
+              <label> Liqueur </label>
+              <input type="checkbox" />
+              <label> Others </label>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <h5>FILTER BY INGREDIENTS</h5>
+          <div>
+            <div>
+              <input type="checkbox" />
+              <label> Rum </label>
+              <input type="checkbox" />
+              <label> Vodka </label>
+              <input type="checkbox" />
+              <label> Gin </label>
+            </div>
+            <div>
+              <input type="checkbox" />
+              <label> Tequila </label>
+              <input type="checkbox" />
+              <label> Liqueur </label>
+              <input type="checkbox" />
+              <label> Others </label>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <form>
+        <button type="submit">Choose another</button>
+      </form>
+    </div>
+  );
 }
 
-export default Cocktail
+export default Cocktail;
