34e649ebcaed9ceeeb1677213776c5e0bf1376af
Gabriela Meirelles <gb.meirellesmartins@gmail.com>
2022-11-28 12:06:15 +0000

    Articles page done

--- src/pages/Articles.jsx
+++ src/pages/Articles.jsx
@@ -2,7 +2,29 @@ import React from 'react'
 
 function ArticlesPage() {
   return (
-    <div>ArticlesPage</div>
+    <div>
+      <div>
+        <h3>Title</h3>
+        <div>
+        {/* <img></img> */}
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi similique repellendus, quasi necessitatibus quidem sapiente error totam laudantium dignissimos iure! Earum, alias! Quo nulla odio quaerat iste modi nemo omnis.</p>
+        </div>
+      </div>
+      <div>
+        <h3>Title</h3>
+        <div>
+        {/* <img></img> */}
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi similique repellendus, quasi necessitatibus quidem sapiente error totam laudantium dignissimos iure! Earum, alias! Quo nulla odio quaerat iste modi nemo omnis.</p>
+        </div>
+      </div>
+      <div>
+        <h3>Title</h3>
+        <div>
+        {/* <img></img> */}
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi similique repellendus, quasi necessitatibus quidem sapiente error totam laudantium dignissimos iure! Earum, alias! Quo nulla odio quaerat iste modi nemo omnis.</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
