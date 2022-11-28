caca192b102c8258ef76a91aaaf7e357bfa19f67
Gabriela Meirelles <gb.meirellesmartins@gmail.com>
2022-11-28 17:34:54 +0000

    global styled done

---
+++ src/components/styled/Global.styled.jsx
@@ -0,0 +1,12 @@
+import {createGlobalStyle} from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+*{
+    font-family: Arial, Helvetica, sans-serif;
+}
+
+body {
+  padding: 0px;
+  margin: 0px;
+}
+`
